b8bdb495a264d8c9f9430d9f93760538218c62e3
Johan Navarro Cano <21sebas12@gmail.com>
2018-11-03 12:40:17 -0500

    first commit

---
+++ src/main/java/co/com/App/java/App.java
@@ -0,0 +1,20 @@
+package co.com.App.java;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import co.com.ias.model.Persona;
+
+public class App {
+	public static void main(String[] args) {
+		/*
+		Persona p = new Persona();
+		p.setNombres("Johan");
+		System.out.println("Nombre>>"+p.getNombres());
+		*/
+		//Patron de diseño facotria 
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		Persona per = (Persona) context.getBean("persona");
+		System.out.println("Nombre>>"+per.getApellidos());
+	}
+}
